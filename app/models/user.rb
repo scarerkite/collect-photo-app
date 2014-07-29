88e6313ca49b97ffe05ab81ef5ca7e24f9d99f75
scarerkite <scknight101@gmail.com>
2014-07-29 17:41:16 +0100

    Removed confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :timeoutable
+         :recoverable, :rememberable, :trackable, :validatable, :timeoutable
   acts_as_voter
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :role, :bio, :avatar, :remote_avatar_url
